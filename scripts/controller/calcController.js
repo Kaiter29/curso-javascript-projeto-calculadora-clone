1339575b1adf7e0aa7a1b014bb59df2a0020cdae
Alexsander Moura Gonçalves <alexmgoncalves09@gmail.com>
2023-11-10 01:01:45 -0300

    Função adicionar igual

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._locale = 'pt-BR';
         this._operation = [];
         this._displayCalcEl = document.querySelector("#display");
@@ -61,16 +63,33 @@ class CalcController {
         }
     }
 
+    getResult() {
+        console.log('calculo', this._operation);
+        return eval(this._operation.join(""));
+    }
+
     calc() {
 
         let last = '';
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
 
         if (this._operation.length > 3) {
 
             last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length == 3) {
+
+            this._lastNumber = this.getResult(false);
         }
 
-        let result = eval(this._operation.join(""));
+        let result = this.getResult();
         
         if (last == '%') {
 
@@ -87,18 +106,29 @@ class CalcController {
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay() {
+    getLastItem(isOperator = true) {
 
-        let lastNumber;
+        let lastItem;
 
         for (let i = this._operation.length-1; i >= 0; i--) {
-            
-            if (!this.isOperator(this._operation[i])) {
-
-                lastNumber = this._operation[i];
+                
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                    
+                lastItem = this._operation[i];
                 break;
             }
         }
+
+        if (!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber = this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0;
 
